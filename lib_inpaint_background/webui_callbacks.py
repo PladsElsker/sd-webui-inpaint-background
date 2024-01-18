82ba867a1914871c9818883049b44c701ce04dfb
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-01-17 21:30:05 -0500

    move import

--- lib_inpaint_background/webui_callbacks.py
+++ lib_inpaint_background/webui_callbacks.py
@@ -1,6 +1,7 @@
 from modules.scripts import script_callbacks
 from lib_inpaint_background.settings import create_settings_section
 from lib_inpaint_background.ui import InpaintBackgroundTab
+from sdwi2iextender import register_operation_mode
 
 
 def on_ui_settings():
@@ -10,5 +11,4 @@ def on_ui_settings():
 def setup_script_callbacks(enabled):
     script_callbacks.on_ui_settings(on_ui_settings)
     if enabled:
-        from sdwi2iextender import register_operation_mode
         register_operation_mode(InpaintBackgroundTab)
