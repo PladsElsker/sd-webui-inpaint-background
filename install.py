bbaa643ac4faf17aef77d2099db65f83ad416a43
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-04-08 18:44:23 -0400

    bump sdw2i2extender to 0.1.0

--- install.py
+++ install.py
@@ -26,10 +26,10 @@ for dependency in pip_dependencies:
         launch.run_pip(f"install {dependency}", f"{dependency} for sd-webui-inpaint-background")
 
 
-sdwi2iextender_version = "0.0.10"
+minimum_sdwi2iextender_version = "0.1.0"
 if not launch.is_installed('sdwi2iextender'):
-    launch.run_pip(f'install sdwi2iextender=={sdwi2iextender_version}', f"sdwi2iextender for sd-webui-inpaint-difference")
+    launch.run_pip(f'install sdwi2iextender', f"sdwi2iextender for sd-webui-inpaint-difference")
 else:
     current_version = pkg_resources.get_distribution("sdwi2iextender").version
-    if pkg_resources.parse_version(current_version) != pkg_resources.parse_version(sdwi2iextender_version):
-        launch.run_pip(f'install sdwi2iextender=={sdwi2iextender_version}', f"sdwi2iextender=={sdwi2iextender_version} for sd-webui-inpaint-difference")
+    if pkg_resources.parse_version(current_version) < pkg_resources.parse_version(minimum_sdwi2iextender_version):
+        launch.run_pip(f'install sdwi2iextender=={minimum_sdwi2iextender_version}', f"sdwi2iextender=={minimum_sdwi2iextender_version} for sd-webui-inpaint-difference")
