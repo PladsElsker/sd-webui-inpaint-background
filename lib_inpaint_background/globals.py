db31992779dc1eadf5c6a57d4d3ac53ff236c90f
Plads <34081873+John-WL@users.noreply.github.com>
2024-04-08 18:54:15 -0400

    Move boilerplate stuff to sdwi2iextender (#6)
    * Move boilerplate stuff to sdwi2iextender
    
    * bump sdw2i2extender to 0.1.0

--- lib_inpaint_background/globals.py
+++ lib_inpaint_background/globals.py
@@ -2,11 +2,6 @@ from modules.shared import opts
 
 
 class BackgroundGlobals:
-    tab_index = None
-
-    base_image = None
-    generated_mask = None
-    
     is_extension_enabled = opts.data.get('inpaint_background_enabled', True)
     show_image_under_mask = opts.data.get('inpaint_background_show_image_under_mask', True)
     mask_brush_color = opts.data.get('inpaint_background_mask_brush_color', '#ffffff')
